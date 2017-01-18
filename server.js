18afba14139a75924b2f4cadba7b00c99deff5b1
Dan Fletcher <dan@danfletcher.com>
2017-01-18 15:05:43 -0500

    name strings

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
-var page = url.parse(req.url).pathname;
-console.log(page);
-res.writeHead(200, {"Content-Type": "text/plain"});
-if (page == '/') {
-res.write('You\'re at the reception desk. How can I help you?');
-}
-else if (page == '/basement') {
-res.write('You\'re in the wine cellar. These bottles are mine!');
-}
-else if (page == '/floor/1/bedroom') {
-res.write('Hey, this is a private area!');
-}
-else {
-	res.writeHead(404);
-	res.write('404. There\'s nothing here for you to see.');
-}
-res.end();
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if ('firstname' in params && 'lastname' in params) {
+        res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
+    }
+    else {
+        res.write(''You do have a first name and a last name, don\'t you?');
+    }
+    res.end();
 });
 server.listen(8080);
